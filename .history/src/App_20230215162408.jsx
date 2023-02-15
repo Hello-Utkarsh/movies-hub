b5a3b7d899a9cd6d22b50d7973a13d86043cff9b
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-02-15 16:34:22 +0530

    Migrated to React + Vite

---
+++ .history/src/App_20230215162408.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      
+    </div>
+    <h1>This is </h1>
+  )
+}
+
+export default App
--- README.md
+++
@@ -1,2 +0,0 @@
-# Movies-Hub
-An Open Source platform for movies and tv shows .. This open source project is an insipiration taken from Netflix
