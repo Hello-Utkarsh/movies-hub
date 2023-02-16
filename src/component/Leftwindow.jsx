01bfb9010ee4bf6939399f37da56bc19fa6a17a0
Utkarsh Patel <pateluttkarsh697@gmail.com>
2023-02-16 19:30:16 +0530

    completed the leftwindow

--- src/component/Leftwindow.jsx
+++ src/component/Leftwindow.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 
 export default function leftwindow() {
 
-  // for changing the color of text and icon on hover
-  let changecolor = ()=>{console.log("Hello")}
- 
   return (
     <div className='w-4/12 bg-black h-[100vh]'>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
@@ -20,24 +17,45 @@ export default function leftwindow() {
         </div>
       </div>
       <div className='px-10'>
-        <h3 className='text-gray-500 flex flex-col text-xl'>News Feed</h3>
-        <div onMouseOver={changecolor()} className='news_feed_content px-1 py-5 flex items-center cursor-pointer'>
-          <span className="icon material-symbols-outlined mt-1 transition ease-in-out text-gray-500 hover:text-red-400 duration-500" style={{ fontSize: '28px' }}>
+        <h3 className='text-gray-500 flex flex-col text-lg'>News Feed</h3>
+        <div className='news_feed_content px-2 py-5 flex items-center cursor-pointer'>
+          <span className="icon material-symbols-outlined mt-1 transition ease-in-out text-gray-500 hover:text-red-400 duration-300" style={{ fontSize: '28px' }}>
             explore
           </span>
-          <h2 className='icon-text text-gray-500 text-xl px-5 hover:text-gray-300'>Browse</h2>
+          <h2 className='icon-text text-gray-300 text-xl px-5 w-fit hover:text-gray-300'>Browse</h2>
         </div>
         <div className='news_feed_content px-1 py-5 flex items-center cursor-pointer'>
-          <span className="icon material-symbols-outlined mt-1 transition ease-in-out text-gray-500 hover:text-red-400 duration-500" style={{ fontSize: '28px' }}>
+          <span className="icon material-symbols-outlined mt-1 transition ease-in-out text-gray-500 hover:text-red-400 duration-300" style={{ fontSize: '28px' }}>
             favorite
           </span>
-          <h2 className='icon-text text-gray-500 text-xl px-5 hover:text-gray-300'>Watchlist</h2>
+          <h2 className='icon-text text-gray-300 text-xl px-5 hover:text-gray-300'>Watchlist</h2>
         </div>
         <div className='news_feed_content px-1 py-5 flex items-center cursor-pointer'>
-          <span className="icon material-symbols-outlined mt-1 transition ease-in-out text-gray-500 hover:text-red-400 duration-500" style={{ fontSize: '28px' }}>
+          <span className="icon material-symbols-outlined mt-1 transition ease-in-out text-gray-500 hover:text-red-400 duration-300" style={{ fontSize: '28px' }}>
             calendar_month
           </span>
-          <h2 className='icon-text text-gray-500 text-xl px-5 hover:text-gray-300'>Comg Soon</h2>
+          <h2 className='icon-text text-gray-300 text-xl px-5 hover:text-gray-300'>Comg Soon</h2>
+        </div>
+      </div>
+      <div className='px-10 py-10'>
+        <h3 className='text-gray-500 flex flex-col text-lg'>Following</h3>
+        <div className='px-2'>
+          <div className=' py-5 flex'>
+            <img className='w-[32px] h-[32px] rounded-full' src="https://images.pexels.com/photos/762527/pexels-photo-762527.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+            <h4 className='text-gray-500 px-5'>UserNAme</h4>
+          </div>
+          <div className=' py-5 flex'>
+            <img className='w-[32px] h-[32px] rounded-full' src="https://images.pexels.com/photos/762527/pexels-photo-762527.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+            <h4 className='text-gray-500 px-5'>UserNAme</h4>
+          </div>
+          <div className=' py-5 flex'>
+            <img className='w-[32px] h-[32px] rounded-full' src="https://images.pexels.com/photos/762527/pexels-photo-762527.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+            <h4 className='text-gray-500 px-5'>UserNAme</h4>
+          </div>
+          <div className=' py-5 flex'>
+            <img className='w-[32px] h-[32px] rounded-full' src="https://images.pexels.com/photos/762527/pexels-photo-762527.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+            <h4 className='text-gray-500 px-5'>UserNAme</h4>
+          </div>
         </div>
       </div>
     </div>
